2f3d62386bed9e229f64b16f3984543b4591e567
NAYRA FONSECA DOS SANTOS <nayrasantos0842@digitalcollege.internal>
2025-10-28 21:38:39 -0300

    node fse path

--- modulo.js
+++ modulo.js
@@ -1,9 +1,12 @@
 const os = require('os');
-
+const chalk = require ('chalk')
+const {converterByteToMb }= require('./util/conversor.js')
 let tipo = os.type();
 const momoria_livre = os.freemem()
 
+console.log(chalk.blue.bold);
 
 console.log(`Tipo ${tipo}`);
 console.log(`Tipo ${os.release()}`);
 console.log(`Tipo ${momoria_livre}`);
+console.log(`Tipo ${converterByteToMb(momoria_livre)}MB`);''
